aa755693571a4a74cee7094c4c07cbabfa09db38
Eduuuuuuuuuuuu <125254483+Eduuuuuuuuuuuu@users.noreply.github.com>
2023-10-09 09:25:01 +0200

    ex3

--- Echo Jammer/Echo Jammer/Modules/Levels/Scena_exercisi3.cpp
+++ Echo Jammer/Echo Jammer/Modules/Levels/Scena_exercisi3.cpp
@@ -72,6 +72,17 @@ bool Scena_Exercisi3::Start()
 
 Update_Status Scena_Exercisi3::Update() {
 
+	if (App->input->keys[SDL_SCANCODE_UP] == Key_State::KEY_REPEAT && !_start) {
+		_velocitatInicial_X++;
+		_velocitatInicial_Y++;
+		LOG("SHOOT!");
+	}
+	if (App->input->keys[SDL_SCANCODE_DOWN] == Key_State::KEY_REPEAT && !_start) {
+		_velocitatInicial_X--;
+		_velocitatInicial_Y--;
+		LOG("SHOOT!");
+	}
+	
 	if (App->input->keys[SDL_SCANCODE_F3] == Key_State::KEY_DOWN && !_start) {
 		_boom = false;
 		_start = true;
