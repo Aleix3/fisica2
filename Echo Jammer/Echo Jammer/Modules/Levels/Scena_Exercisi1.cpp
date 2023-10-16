a5091206410a0c00703c8e323d2ff0303563b531
Ferran-LX <115022293+Ferran-LX@users.noreply.github.com>
2023-10-16 10:09:12 +0200

    bla

--- Echo Jammer/Echo Jammer/Modules/Levels/Scena_Exercisi1.cpp
+++ Echo Jammer/Echo Jammer/Modules/Levels/Scena_Exercisi1.cpp
@@ -24,6 +24,90 @@ bool Scena_Exercisi1::Start(){
 }
 
 Update_Status Scena_Exercisi1::Update() {
+	if (app->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT)
+	{
+		boxX -= 4; // Move left
+	}
+	if (app->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT)
+	{
+		boxX += 4; // Move right
+	}
+
+
+
+	//Get the size of the window
+	uint windowW, windowH;
+	app->win->GetWindowSize(windowW, windowH);
+
+
+
+	//Get the size of the texture
+	uint texW, texH;
+	app->tex->GetSize(_caja, texW, texH);
+	app->tex->GetSize(img2, texW, texH);
+
+
+
+
+	if (boxX > (plX - 300) && boxX < plX + 120)
+	{
+		plY = 2000;
+		plY2 = 550;
+
+
+		if (direcction < boxX)
+		{
+			app->render->DrawTexture(_planta2, plX2, plY2, NULL, NULL, 60);
+			app->render->DrawTexture(_caja, boxX, boxY);
+		}
+
+		else if (direcction > boxX)
+		{
+			app->render->DrawTexture(_planta2, plX2, plY2, NULL, NULL, -60);
+			app->render->DrawTexture(_caja, boxX, boxY);
+		}
+
+		else if (direcction = boxX)
+		{
+			app->render->DrawTexture(_planta2, plX2, plY2);
+			app->render->DrawTexture(_caja, boxX, boxY);
+		}
+
+
+
+
+	}
+	else if (boxX > (plX + 100) || boxX < plX + 120)
+	{
+		plY = 2000;
+		plY2 = 500;
+		app->render->DrawTexture(_planta2, plX2, plY2);
+		app->render->DrawTexture(_caja, boxX, boxY);
+	}
+
+
+
+	// Renders the image in the center of the screen
+	/*app->render->DrawTexture(img, windowW /1 - texW / 1, windowH /1 - texH / 1);*/
+
+
+
+		// Renders the image with scaled dimensions
+	else
+	{
+		app->render->DrawTexture(_planta, plX, plY);
+
+		app->render->DrawTexture(_caja, boxX, boxY);
+	}
+
+
+
+
+	direcction = boxX;
+
+	app->render->DrawTexture(_suelo, 0, 620);
+
+
 
 	
 	return Update_Status::UPDATE_CONTINUE;
