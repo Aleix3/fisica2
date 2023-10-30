0861bea2063540e5de311572d70687fcffb6a1d6
Ferran-LX <115022293+Ferran-LX@users.noreply.github.com>
2023-10-30 12:15:42 +0100

    Projecte per fer pinball

---
+++ citm_desvj_project_template-L8_Map_Metadata_Solution/Game/Source/Scene.cpp
@@ -0,0 +1,131 @@
+#include "App.h"
+#include "Input.h"
+#include "Textures.h"
+#include "Audio.h"
+#include "Render.h"
+#include "Window.h"
+#include "Scene.h"
+#include "Map.h"
+#include "Item.h"
+
+#include "Defs.h"
+#include "Log.h"
+
+Scene::Scene() : Module()
+{
+	name.Create("scene");
+}
+
+// Destructor
+Scene::~Scene()
+{}
+
+// Called before render is available
+bool Scene::Awake(pugi::xml_node config)
+{
+	LOG("Loading Scene");
+	bool ret = true;
+
+	//Instantiate the player using the entity manager
+	//Get player paremeters
+	player = (Player*) app->entityManager->CreateEntity(EntityType::PLAYER);
+	//Assigns the XML node to a member in player
+	player->config = config.child("player");
+
+	//Get the map name from the config file and assigns the value in the module
+	app->map->name = config.child("map").attribute("name").as_string();
+	app->map->path = config.child("map").attribute("path").as_string();
+
+	// iterate all items in the scene
+	// Check https://pugixml.org/docs/quickstart.html#access
+	for (pugi::xml_node itemNode = config.child("item"); itemNode; itemNode = itemNode.next_sibling("item"))
+	{
+		Item* item = (Item*)app->entityManager->CreateEntity(EntityType::ITEM);
+		item->parameters = itemNode;
+	}
+	
+	return ret;
+}
+
+// Called before the first frame
+bool Scene::Start()
+{
+	// NOTE: We have to avoid the use of paths in the code, we will move it later to a config file
+	img = app->tex->Load("Assets/Textures/test.png");
+	
+	//Music is commented so that you can add your own music
+	//app->audio->PlayMusic("Assets/Audio/Music/music_spy.ogg");
+
+	//Get the size of the window
+	app->win->GetWindowSize(windowW, windowH);
+
+	//Get the size of the texture
+	app->tex->GetSize(img, texW, texH);
+
+	textPosX = (float)windowW / 2 - (float)texW / 2;
+	textPosY = (float)windowH / 2 - (float)texH / 2;
+
+	_textura_aspid = app->tex->Load("Assets/Textures/aspid.png");
+
+
+	for (int i = 0; i < 4; i++)
+		_aspidAnimation.PushBack({ 58 * i + 2, 0, 58, 57 });
+	_aspidAnimation.loop = true;
+	_aspidAnimation.speed = 0.2f;
+
+	_rectAspid = { 100, 100, 58, 57 };
+
+	return true;
+}
+
+// Called each loop iteration
+bool Scene::PreUpdate()
+{
+	return true;
+}
+
+// Called each loop iteration
+bool Scene::Update(float dt)
+{
+	//L02 DONE 3: Make the camera movement independent of framerate
+	float camSpeed = 1; 
+
+	if(app->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
+		app->render->camera.y -= (int)ceil(camSpeed * dt);
+
+	if(app->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
+		app->render->camera.y += (int)ceil(camSpeed * dt);
+
+	if(app->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
+		app->render->camera.x -= (int)ceil(camSpeed * dt);
+
+	if(app->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
+		app->render->camera.x += (int)ceil(camSpeed * dt);
+
+	// Renders the image in the center of the screen 
+	//app->render->DrawTexture(img, (int)textPosX, (int)textPosY);
+
+	return true;
+}
+
+// Called each loop iteration
+bool Scene::PostUpdate()
+{
+	bool ret = true;
+
+	if(app->input->GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
+		ret = false;
+
+	_aspidAnimation.Update();
+	app->render->DrawTexture(_textura_aspid, _rectAspid.x, _rectAspid.y, &_aspidAnimation.GetCurrentFrame());
+
+	return ret;
+}
+
+// Called before quitting
+bool Scene::CleanUp()
+{
+	LOG("Freeing scene");
+
+	return true;
+}
