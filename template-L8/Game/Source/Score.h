0c89c083c3b8734366d9705e9c705b5dfafdb436
Eduuuuuuuuuuuu <eduard.garcia.tobajas@estudiantat.upc.edu>
2023-11-17 14:26:01 +0100

    a

---
+++ template-L8/Game/Source/Score.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class Score
+{
+public:
+    Score();
+    ~Score();
+
+    void AddPoints(int points);
+    void Reset();
+    int GetScore() const;
+
+private:
+    int score;
+};
+
