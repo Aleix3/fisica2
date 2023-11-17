05e61810c338273c5cdd5fe6c4c9690db1cddc8b
Eduuuuuuuuuuuu <eduard.garcia.tobajas@estudiantat.upc.edu>
2023-11-17 14:31:10 +0100

    Cambio de sitio Score

--- template-L8/Game/Source/Score.cpp
+++ template-L8/Game/Source/Score.cpp
@@ -1,24 +1,18 @@
 #include "Score.h"
-
-Score::Score() : score(0) 
+Score::Score() : score(0)
 {
 }
-
 Score::~Score()
 {
-
 }
-
 void Score::AddPoints(int points)
 {
     score += points;
 }
-
 void Score::Reset()
 {
     score = 0;
 }
-
 int Score::GetScore() const
 {
     return score;
--- template-L8/Game/Source/Score.h
+++ template-L8/Game/Source/Score.h
@@ -5,12 +5,9 @@ class Score
 public:
     Score();
     ~Score();
-
     void AddPoints(int points);
     void Reset();
     int GetScore() const;
-
 private:
     int score;
 };
-
