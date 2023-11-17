2b13e208280180db20b219e5175b8f5abe54801d
Eduuuuuuuuuuuu <eduard.garcia.tobajas@estudiantat.upc.edu>
2023-11-17 14:09:54 +0100

    score

--- template-L8/Game/Score.cpp
+++ template-L8/Game/Score.cpp
@@ -1,25 +1,25 @@
 #include "Score.h"
 
-Score::Score() : score(0) // Inicializa la puntuación a 0 en el constructor
+Score::Score() : score(0) 
 {
 }
 
 Score::~Score()
 {
-    // Aquí puedes liberar cualquier recurso que Score pueda haber adquirido
+
 }
 
 void Score::AddPoints(int points)
 {
-    score += points; // Añade 'points' a 'score'
+    score += points;
 }
 
 void Score::Reset()
 {
-    score = 0; // Resetea 'score' a 0
+    score = 0;
 }
 
 int Score::GetScore() const
 {
-    return score; // Devuelve la puntuación actual
+    return score;
 }
